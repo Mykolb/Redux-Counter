6358222f10e25f8cb14e2e89726fe0c27370fff6
Mykol Benning <mykolbenning@MBP13inhMid2012.fios-router.home>
2019-03-18 18:01:58 -0400

    Initial commit, filled in actions and reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -10,10 +10,16 @@ const initialState = {
 // by our action creator. What does the reducer
 // need to do with the count in each case?
 export default (state = initialState, action) => {
+  console.log(action);
   switch (action.type) {
     case INCREMENT:
+    return {
+      count: state.count + 1
+    }
     // Fill in the body of this case
     case DECREMENT:
+    return {
+     count: state.count - 1    }
     // Fill in the body of this case
     default:
       return state;
